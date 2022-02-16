789432f0c8fe60c289580585c5ad7d99dbd2f592
AndriyYuzvenko <anbriy890@gmail.com>
2022-02-16 12:40:18 +0200

    classwork

--- classwork1/task3/app.js
+++ classwork1/task3/app.js
@@ -5,11 +5,16 @@ fs.readdir(path.join(__dirname, 'fiels'), (err, data) => {
     if (err) console.log(err)
     data.map(item => {
         const status = item.endsWith('txt')
-        if (status !== true) {
-            fs.rename(path.join(__dirname, 'task', item),
-                path.join(__dirname, 'task', 'new' + item), (err) => {
+        if (status === true) {
+            fs.truncate(path.join(__dirname, 'fiels', item), err => {
+                if (err) console.log(err);
+            });
+        } else {
+            fs.rename(path.join(__dirname, 'fiels', item)
+                , path.join(__dirname, 'fiels', 'new' + item), (err) => {
                     if (err) console.log(err)
                 })
         }
     })
+
 })
