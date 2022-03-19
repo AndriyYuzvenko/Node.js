b6397e8ea717a0cfdbadcaa0c1a2c78725225337
AndriyYuzvenko <anbriy890@gmail.com>
2022-03-19 09:09:55 +0200

    homework

--- homework6/src/repositories/posts/postRepository.interface.ts
+++ homework6/src/repositories/posts/postRepository.interface.ts
@@ -3,5 +3,5 @@ import { IPost } from '../../entity/post';
 export interface IPostRepository {
     postPosts(post:IPost):Promise<IPost>;
     getPosts(userId:string):Promise<IPost | {}>;
-    patchPost(postId:string, post:IPost):Promise<string | {}>;
+    patchPost(postId:string, post:IPost):Promise<any>;
 }
